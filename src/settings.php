d41b7699e38cd74a067ba3f07c035c794e6fc5d6
RainyRuRu <believeu123@gmail.com>
2018-12-19 16:26:54 +0800

    Add DB Setting

--- src/settings.php
+++ src/settings.php
@@ -23,5 +23,17 @@ return [
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
+
+        // DB setting
+        'db' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', 'localhost'),
+            'database' => env('DB_DATABASE', 'database'),
+            'username' => env('DB_USERNAME', 'username'),
+            'password' => env('DB_PASSWORD', 'password'),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+        ]
     ],
 ];
