fb0a874d8bade92c13b5f1a3e441ccdea68152f0
RainyRuRu <believeu123@gmail.com>
2018-12-20 17:24:55 +0800

    Add BaseController and adjust return format

--- src/controllers/ShopController.php
+++ src/controllers/ShopController.php
@@ -5,11 +5,11 @@ use App\Models\Shop;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-class ShopController
+class ShopController extends BaseController
 {
     public function get(Request $request, Response $response)
     {
         $shops = Shop::query()->select(['id', 'name'])->get()->toArray();
-        return $response->withStatus(200)->withJson($shops);
+        return $this->successReturn($response, $shops);
     }
 }
