9cfaeff884eef1e5e72762a74eee1595a06a784c
RainyRuRu <believeu123@gmail.com>
2018-12-20 16:38:42 +0800

    Add Shop model

---
+++ src/models/Shop.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Shop extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'shop';
+    protected $dateFormat = 'U';
+}
